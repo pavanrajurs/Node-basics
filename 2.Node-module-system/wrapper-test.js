9cfdadd0e3c31b4fefeb55d2244b27d7a9054d0f
pavanrajurs <pavanrajurs888@gmail.com>
2025-06-30 11:27:36 +0530

    intial commit

--- 2.Node-module-system/wrapper-test.js
+++ 2.Node-module-system/wrapper-test.js
@@ -1,10 +1,9 @@
-console.log("FILE NAME",__filename);
-console.log("DIRECTORY NAME",__dirname);
-console.log(exports);
-console.log(module);
-console.log(require);
+console.log("FILE NAME", __filename);
+console.log("DIRECTORY NAME", __dirname);
+console.log("EXPORTS", exports);
+console.log("MODULE", module);
+console.log("REQUIRE", require);
 
-// console.log(require("module").wrapper);
 // (function (exports, require, module, __filename, __dirname) {
 //   const name = "Raj";
 //   console.log(name);
